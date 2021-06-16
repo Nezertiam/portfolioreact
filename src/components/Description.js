89478921571c338ce2d610c1b5835a55e202a62d
Nezertiam <mawyn.nhek@gmail.com>
2021-06-16 15:20:34 +0200

    [NEW] added/profile-pic-from-GithubAPI

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 import StyledSectionOrange from "./StyledSectionOrange"
+import axios from "axios"
 
 const Description = ({ content }) => {
 
+    const [githubUser, setGithubUser] = useState([])
+
+    useEffect(() => {
+        axios.get("https://api.github.com/users/nezertiam").then((response) => {
+            setGithubUser(response)
+        })
+    }, [])
+
     return (
         <StyledSection className="description">
+
+            <figure className="description__figure">
+                <img src={ githubUser.data.avatar_url } className="description__image" />
+            </figure>
+
             {
                 content.title &&
                 <h1 className="description__title" dangerouslySetInnerHTML={{ __html: content.title }}/>
@@ -26,17 +41,27 @@ const Description = ({ content }) => {
 
 const StyledSection = styled(StyledSectionOrange)`
     .description{
-        &__title, &__content{
+        &__title, &__content, &__figure{
             text-align: center;
+        }
+        &__image{
+            height: 100%;
+            border: 3px solid black;
+            border-radius: 50%;
+            background-color: lightsteelblue;
+        }
+        &__figure{
+            height: 300px;
+            margin-bottom: 2rem;
+        }
+        &__title{
+            font-size: 1.75rem;
+            margin-bottom: 2rem;
         }
         &__content{
             max-width: var(--xs);
             margin: 0 auto;
             font-size: 1.2rem;
-        }
-        &__title{
-            font-size: 1.75rem;
-            margin-bottom: 2rem;
         }
     }
 `
